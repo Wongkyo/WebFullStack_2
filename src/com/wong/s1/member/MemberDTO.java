ec58683a1a8ccda3782f3e86617a6a8e3eb02371
WonKyoSeo <swkgold@Gmail.com>
2021-03-15 23:27:55 +0900

    wong2

---
+++ src/com/wong/s1/member/MemberDTO.java
@@ -0,0 +1,48 @@
+package com.wong.s1.member;
+
+public class MemberDTO {
+
+	//모든 멤버변수는 private
+	//변수명과 데이타타입은 컬럼과 동일하게
+	//getter, setter
+	//기본생성자 1개
+	
+	private String id;
+	private String pw;
+	private String name;
+	private String mobile;
+	private String email;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
